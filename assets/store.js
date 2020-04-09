7c3c1002eaa64e00694f5924d1d991b85287015d
claudia <mail@claudiapiccioni.it>
2020-04-09 15:25:35 +0200

    In order to update the total price, I created the function updateCartTotal that could find each item price and multiply that by the quantity, and add the result for each row, showing the total. At the moment I found the way to get the price information from the price element

--- assets/store.js
+++ assets/store.js
@@ -1,13 +1,30 @@
 let removeCartItemButtons = document.getElementsByClassName("btn-danger")
 
-console.log(removeCartItemButtons)
+// console.log(removeCartItemButtons)
 
 for (let i = 0; i < removeCartItemButtons.length; i += 1) {
   let button = removeCartItemButtons[i]
 
-  button.addEventListener('click', function () {
+  button.addEventListener('click', function (event) {
     let buttonClicked = event.target
     buttonClicked.parentElement.parentElement.remove()
+    updateCartTotal()
 
   })
+}
+
+function updateCartTotal() {
+  let cartItemContainer = document.getElementsByClassName("cart-items")[0]
+  let cartRows = cartItemContainer.getElementsByClassName("cart-row")
+  // console.log(cartItemContainer, cartRows)
+
+  for (let i = 0; i < cartRows.length; i += 1) {
+    let cartRow = cartRows[i]
+    let priceElement = cartRow.getElementsByClassName("cart-price")[0]
+    let quantityElement = cartRow.getElementsByClassName("cart-quantity-input")[0]
+    // console.log(priceElement, quantityElement)
+    let price = priceElement.innerText.replace("€", "")
+    console.log(price)
+
+  }
 }
